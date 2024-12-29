07f99523e6587b582f08e5cadc36790f164d9a7c
101-cyber <mksvanvan@gmail.com>
2024-12-29 11:33:44 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,25 @@
 import os
-from config import bot
-from commands import *
+import sys
+from pathlib import Path
+from dotenv import load_dotenv
 
-# Démarrage du bot
-bot.run(os.getenv('DISCORD_TOKEN'))
+# Ajout du chemin de base pour les imports
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+from script.discordbot.CAF.config import bot
+from script.discordbot.CAF.keep_alive import keep_alive
+from script.discordbot.CAF.commands import *  # Charge les commandes
+
+# Charger les variables d'environnement
+dotenv_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=dotenv_path)
+token = os.getenv('DISCORD_TOKEN')
+
+if not token:
+    raise ValueError("Le token Discord est introuvable. Vérifiez le fichier .env et la variable DISCORD_TOKEN.")
+
+# Garder le bot en ligne
+keep_alive()
+
+# Lancer le bot
+bot.run(token)
