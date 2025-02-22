48ac990daf4a4665730a9e7f136b653d7f85ed1d
101-cyber <mksvanvan@gmail.com>
2025-02-22 20:57:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,6 @@
 import os
 from dotenv import load_dotenv
 
-# Charge les variables d'environnement depuis le fichier .env
 load_dotenv()
 
-# Récupère le token du bot depuis les variables d'environnement
 TOKEN = os.getenv("DISCORD_TOKEN")
